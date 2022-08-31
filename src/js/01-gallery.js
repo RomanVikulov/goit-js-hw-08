733b0476013013957f834cb16da9a25e822f91a2
Roman <romanuch06.88@gmail.com>
2022-08-31 16:22:13 +0300

    finished 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,7 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
