a30f2f67fb47c764d8ea2bd9410bedd03a520f38
Roman <romanuch06.88@gmail.com>
2022-09-04 12:45:39 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[type="email"]'),
+  textarea: document.querySelector('textarea'),
+};
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+let formData = localStorage.getItem(LOCALSTORAGE_KEY)
+  ? JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY))
+  : {};
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateForm();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('formData', formData);
+
+  onResetForm();
+}
+
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateForm() {
+  const savedForm = localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (savedForm) {
+    for (const [key, value] of Object.entries(formData)) {
+      refs.form[key].value = value;
+    }
+    console.log('populateForm ~ savedForm', savedForm);
+  }
+}
+
+function onResetForm() {
+  refs.form.reset();
+  formData = {};
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
